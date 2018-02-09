3022d5e7f27cdb489dfd4795a1290ee787bab0a8
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-02-09 10:29:08 +0530

    service added

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
+
+
+  registerService(data){
+    return this._http.post(environment.api_url+"/register",data);
+  }
+
+  loginService(data){
+    return this._http.post(environment.api_url+"/login",data);
+  }
+  logoutService(data){
+    return this._http.get(environment.api_url+"/logout");
+  }
 
 }
