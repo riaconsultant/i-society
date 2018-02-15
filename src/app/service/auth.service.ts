a31a2d829be572d75542e149636d6ee19658a737
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-02-16 01:23:19 +0530

    error block added

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs/Observable';
+import { HttpErrorResponse } from '@angular/common/http/src/response';
 
 @Injectable()
 export class AuthService {
@@ -12,8 +13,8 @@ export class AuthService {
     let data={};
     this.loginService(data).subscribe((result:any)=>{
 
-    },(error)=>{
-      
+    },(error:HttpErrorResponse)=>{
+      console.log("Error",error.error);
     });
   }
   
