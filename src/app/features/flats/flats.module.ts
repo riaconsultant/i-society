9188cf92be83d9e639a49ee4a101afe79ebf539b
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-02-19 00:24:52 +0530

    Event Modul Added

---
+++ src/app/features/flats/flats.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FlatsRoutingModule } from './flats-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FlatsRoutingModule
+  ],
+  declarations: []
+})
+export class FlatsModule { }
