266fdb0d7364567ab9c2a3cc9370efbc828d7976
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-04-01 23:56:15 +0530

    Component Added

--- src/app/common/registration/registration.component.ts
+++ src/app/common/registration/registration.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnChanges,OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { User } from '../../model/user';
+import { ProfileService } from '../../service/profile.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent implements OnInit {
-
-  constructor() { }
+export class RegistrationComponent implements OnInit,OnChanges,OnDestroy {
+  model:User
+  registerSubs:Subscription;
+  constructor(private profile:ProfileService) { }
 
   ngOnInit() {
+    this.model = new User();
+    console.log("Init");
+  }
+  ngOnChanges(){
+    console.log("Changes");
+  }
+  ngOnDestroy(){
+    if(this.registerSubs){
+      this.registerSubs.unsubscribe();
+      console.log("Destroy");
+    }
+  }
+  registerMe(form:NgForm){
+    console.log("Register form data",form);
+    if(form.valid){
+      // this.registerSubs=this.profile.registerProfile(form.value).subscribe((result:any)=>{
+
+      // },(error:HttpErrorResponse)=>{
+      //   let errData = error.error;
+      //   console.log("HttpErrorResponse",error);
+      // })
+    }
+    
   }
 
 }
