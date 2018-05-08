6be66b6280d585543c2389cdc9cf01c7dbf0c838
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-05-08 23:10:45 +0530

    User Model Added

---
+++ src/app/model/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    public username:string;
+    public first_name:string;
+    public last_name:string;
+    public email:string;
+    public token?:string;
+    public term:boolean;
+    public status:boolean;
+    public last_login:string;
+    public profile_id?:string;
+    public password?:string;
+}
