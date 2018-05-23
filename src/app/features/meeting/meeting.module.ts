ae7e9c6dd04770498bc99963529108f0c6d82126
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-05-24 00:26:13 +0530

    Meeting Modules added

---
+++ src/app/features/meeting/meeting.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MeetingRoutingModule } from './meeting-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MeetingRoutingModule
+  ],
+  declarations: []
+})
+export class MeetingModule { }
