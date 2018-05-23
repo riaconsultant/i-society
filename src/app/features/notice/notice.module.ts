a914c7b6f9bef3efe725be18d98c5210eb21cd2e
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-05-24 00:27:18 +0530

    notice module added

---
+++ src/app/features/notice/notice.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NoticeRoutingModule } from './notice-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    NoticeRoutingModule
+  ],
+  declarations: []
+})
+export class NoticeModule { }
