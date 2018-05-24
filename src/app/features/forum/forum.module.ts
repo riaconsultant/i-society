f11ebc3145d39523d3f468d420ebf9b8fd34e941
Manoj Chaurasiya <manoj.chaurasiya@gmail.com>
2018-05-24 19:06:23 +0530

    code updates

---
+++ src/app/features/forum/forum.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ForumRoutingModule } from './forum-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ForumRoutingModule
+  ],
+  declarations: []
+})
+export class ForumModule { }
